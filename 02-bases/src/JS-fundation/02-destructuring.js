3b2ced677c6b3fe3203ce01ba18c5b209947d61b
Chema <mrjark7@gmail.com>
2023-10-08 19:24:53 +0200

    added destructuring

---
+++ 02-bases/src/JS-fundation/02-destructuring.js
@@ -0,0 +1,7 @@
+// console.log(process) // process es un proceso de node que siempre se está corriendo en el pc
+// console.log(process.env) // variables de entorno
+
+// puedes desestructurar a través de:
+const { SHELL } = process.env
+
+console.log(SHELL)
--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -2,4 +2,5 @@
 const templateExports = require('./JS-fundation/01-template') // el require es una función especial de NODE -> lo que hace es qeu si escribes en la consola npm start, ejecuta el archivo donde apunta el path
 const { emailTemplate } = require('./JS-fundation/01-template') // lo mismo que lo de arriba
 
+require('./JS-fundation/02-destructuring')
 console.log(templateExports.emailTemplate, emailTemplate)
