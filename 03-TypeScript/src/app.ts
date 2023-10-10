bf6fa8b44fedc27ea0fa4793f5efafc2f668aa70
Chema <mrjark7@gmail.com>
2023-10-10 15:29:13 +0200

    eslint + ts installed

--- 03-TypeScript/src/app.ts
+++ 03-TypeScript/src/app.ts
@@ -1,29 +1,29 @@
 const heroes = [
-  {
-    id: 1,
-    name: 'Ironman',
-    owenr: 'Marvel'
-  },
-  {
-    id: 2,
-    name: 'Superman',
-    owenr: 'DC'
-  },
-  {
-    id: 3,
-    name: 'Spidernan',
-    owenr: 'Marvel'
-  },
-  {
-    id: 4,
-    name: 'Batman',
-    owenr: 'DC'
-  }
+	{
+		id: 1,
+		name: 'Ironman',
+		owenr: 'Marvel'
+	},
+	{
+		id: 2,
+		name: 'Superman',
+		owenr: 'DC'
+	},
+	{
+		id: 3,
+		name: 'Spidernan',
+		owenr: 'Marvel'
+	},
+	{
+		id: 4,
+		name: 'Batman',
+		owenr: 'DC'
+	}
 ]
 
 const findHeroById = (id: number) => {
-  return heroes.find(hero => hero.id === id)
+	return heroes.find(hero => hero.id === id)
 }
 
 const hero = findHeroById(2)
-console.log(hero)
+console.log(hero?.name)
