ebb962c3cb3a3edd0d85bcaf1c8df866c65be835
Chema <mrjark7@gmail.com>
2023-10-15 14:58:57 +0200

    anonymous self-invoked function in app.ts

--- 05-multiplication/src/app.ts
+++ 05-multiplication/src/app.ts
@@ -1,4 +1,14 @@
 import { yarg } from './configs/plugins/yargs.plugin'
 
+// console.log(yarg)
 
-console.log(yarg)
+// para ejecutar código asíncrono desde el root de la app
+(async() => {
+	await main()
+	console.log('ejecutado')
+})()
+
+async function main() {
+	console.log('Main ehe')
+  
+}
