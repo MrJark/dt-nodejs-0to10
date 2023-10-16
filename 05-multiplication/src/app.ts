62582512577229254598956b6fe17dc4b111517a
Chema <mrjark7@gmail.com>
2023-10-16 09:24:04 +0200

    refactoring

--- 05-multiplication/src/app.ts
+++ 05-multiplication/src/app.ts
@@ -1,15 +1,18 @@
 import { yarg } from './configs/plugins/yargs.plugin'
+import { ServerApp } from './presentation/server_app'
 
 // console.log(yarg)
 
 // para ejecutar código asíncrono desde el root de la app
 (async() => {
-	await main()
-	// console.log('2: ejecutado')
+  await main()
+  // console.log('2: ejecutado')
 })()
 
 async function main() {
-	// console.log('1: Main') // este log 1 se ejecuta antes que el 2
-	console.log(yarg)
-	
+  // console.log('1: Main') // este log 1 se ejecuta antes que el 2
+  const {b: base, l: limit, s: showTable} = yarg
+  // console.log(yarg)
+
+  ServerApp.run({ base, limit, showTable})
 }
