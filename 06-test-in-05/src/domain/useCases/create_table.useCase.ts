6dbd8fdc96694db68358c7818e2051bc39bf8140
Chema <mrjark7@gmail.com>
2023-10-17 16:58:43 +0200

    fiest test: create_table.useCse

--- 06-test-in-05/src/domain/useCases/create_table.useCase.ts
+++ 06-test-in-05/src/domain/useCases/create_table.useCase.ts
@@ -10,18 +10,20 @@ export interface CreateTableOptions {
 }
 
 export class CreateTable implements CreateTableUseCase {
-  
-  constructor(
+
+  constructor (
     /**
      * DI: dependency injection 
     */
-  ){}
+  ) { }
 
   // también se suele poner 'run' en vez de execute
-  execute({ base, limit = 10 }: CreateTableOptions ){
+  execute( { base, limit = 10 }: CreateTableOptions ) {
     let outputMessage = ''
     for ( let i = 1; i <= limit; i++ ) {
-      outputMessage += `${base} x ${i} = ${base * i}\n` // lo último, ' \n ', es un salto de linea
+      outputMessage += `${base} x ${i} = ${base * i}` //\n lo último, ' \n ', es un salto de linea
+
+      if ( i < limit ) outputMessage += '\n' // añadido aquí y eliminado en la de arriba para poder probar mejor el test
     }
 
     return outputMessage
---
+++ 06-test-in-05/tests/domain/useCases/create_table.useCase.test.ts
@@ -0,0 +1,37 @@
+import { CreateTable } from '../../../src/domain/useCases/create_table.useCase';
+
+
+describe( 'domain/useCases/create_table.useCase', () => {
+  test( 'should create table with default value', () => {
+    const createNewTable = new CreateTable()
+    const table = createNewTable.execute( { base: 2 } )
+    const rows = table.split( '\n' ).length // corta por el salto de línea
+    // console.log( table )
+    // console.log( rows )
+
+    expect( createNewTable ).toBeInstanceOf( CreateTable )
+    expect( table ).toContain( '2 x 2 = 4' )
+    expect( table ).toContain( '2 x 10 = 20' )
+    expect( rows ).toBe( 10 )
+
+  } )
+
+  test( 'Task: should create table with custom values ✅', () => {
+    const options = {
+      limit: 20,
+      base: 3
+    }
+
+    const createNewTable = new CreateTable()
+    const table = createNewTable.execute( options )
+    const rows = table.split( '\n' ).length
+    console.log( table );
+
+
+    expect( table ).toContain( '3 x 8 = 24' )
+    expect( table ).toContain( '3 x 16 = 48' )
+    expect( table ).toContain( '3 x 20 = 60' )
+    expect( rows ).toBe( 20 )
+
+  } )
+} )
