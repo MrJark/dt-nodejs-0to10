0ef2bb5c46f509673fe3027ce0ccfa03bc8ff88f
Chema <mrjark7@gmail.com>
2023-10-19 18:29:24 +0200

    testin app.ts

--- 06-test-in-05/tests/app.test.ts
+++ 06-test-in-05/tests/app.test.ts
@@ -1,6 +1,23 @@
 
-describe( 'App.ts', () => {
-  test( 'should be true', () => {
-    expect( true ).toBe( true )
+// process.argv = [ 'node', 'app.ts', '-b', '10' ]
+// import '../src/app'
+// una froma de hacer el mock 👆🏽👆🏽
+import { ServerApp } from '../src/presentation/server_app'
+
+describe( 'Test App.ts', () => {
+  test( 'should call Server.run with values', async () => {
+    const serverRunMock = jest.fn()
+    ServerApp.run = serverRunMock
+    process.argv = [ 'node', 'app.ts', '-b', '10', '-l', '5', '-n', 'test-file', '-d', 'test-destination' ]
+
+    await import( '../src/app' )
+
+    expect( serverRunMock ).toHaveBeenCalledWith( {
+      base: 10,
+      fileDestination: "test-destination",
+      fileName: "test-file",
+      limit: 5,
+      showTable: false
+    } )
   } )
 } )
