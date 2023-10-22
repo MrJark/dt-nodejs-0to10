4bb0221e7edb1aab4d691f2f19e3db01445e3c4d
Chema <mrjark7@gmail.com>
2023-10-22 11:01:10 +0200

    log.repository and log.datasource and its abstract classes

---
+++ 07-NOC/src/domain/datasource/log.datasource.ts
@@ -0,0 +1,8 @@
+import { LogEntity, LogSeverityLevel } from "../entities/log.entity";
+
+// se le coloca abstract para que nadie pueda crear instancias del LogDatasource. Estas calses abstractas permiten obligar el comportamiento sobre otras clases
+export abstract class LogDatasource {
+  abstract saveLog( log: LogEntity ): Promise<void> // es obligatorio que lo implemente por el abstract
+  abstract getLog( severityLevel: LogSeverityLevel ): Promise<LogEntity[]>
+  // si no complen las dos condiciones abstract no serán soluciones válidas para el LogDatasource
+}
---
+++ 07-NOC/src/domain/datasource/whats_it.md
@@ -0,0 +1,7 @@
+# Data source
+
+Son las reglas de negocio de como quieres que funcionen los datos
+
+esto va a contener el origen de los datos. Puede ser una base de datos, postgress, mongo.
+
+Aquí se crean las clases abstractac o interfaces que hacen falta para poner las reglas de como queires que funcionen las entidades
