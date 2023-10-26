727f7bafbe2374a8a6ddeb556e03e862a4527276
Chema <mrjark7@gmail.com>
2023-10-26 15:10:16 +0200

    added .yml

--- 10-NOC_MongoDB_PostgreSQL/src/presentation/server.ts
+++ 10-NOC_MongoDB_PostgreSQL/src/presentation/server.ts
@@ -18,7 +18,7 @@ export class Server {
   public static start() {
     // es un método público ( aunque todos los son a no ser que lo especifiques )
     // la palabra static se pone para hacer referencia al start con solo poner el punto -> Server.start sino sería muchop más largo la llamada del método start
-    console.log( 'Server started...' );
+    console.log( 'Server started...!!' );
     // console.log( envs.MAILER_EMAIL );
 
 
